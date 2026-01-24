fa6f2abf694ce32f043c6164f6664b5865ed58f2
AmineMOULAI <moulaiamine01@gmail.com>
2026-01-24 16:13:09 +0100

    Create project structure / Web App (first draft)

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st 
+import os
+
+with st.container(border=False, horizontal =True,horizontal_alignment = "center", vertical_alignment="top"):
+    st.image(os.path.join(os.getcwd(), "WisFast.png"), width=300)
+    #st.title("WisFast")
+    st.header("Smart search in your books")
+
+def main():
+
+    file = st.file_uploader("Upload your book")
+
+    query = st.text_input("What are you looking for ...?", "What are you looking for ...?", label_visibility="hidden")
+    print(query)
+
+    st.divider()
+
+    st.feedback("stars")
+
+
+def book1():
+    st.title("Book 1")
+
+def book2():
+    st.title("Book 2")
+
+def book3():
+    st.title("Book 3")
+
+pages = {
+    "main" : main,
+    "Book 1" : book1, 
+    "Book 2 " : book2, 
+    "Book 3" : book3
+         }
+
+selected_page = st.sidebar.selectbox("Choose a Book", options=pages.keys())
+print(selected_page)
+pages[selected_page]()
